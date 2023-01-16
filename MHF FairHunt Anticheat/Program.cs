9e578817f1b1df05b57ce823b49379d2712b7ae8
wrole <wrole@VKTR-DSK>
2023-01-15 21:39:10 -0300

    Add project files.

---
+++ MHF FairHunt Anticheat/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace MHF_FairHunt_Anticheat
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
